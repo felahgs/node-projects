f4582eefffa9b4d191820e25f0cdf4731815e426
Felipe Souza <fgsouza93@gmail.com>
2024-10-06 12:49:26 -0300

    add paths, html pages serving and handle 404

--- hello-express/app.js
+++ hello-express/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -11,8 +12,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Express also has a bodyParser implmented on its core.
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+app.use("/", shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "not-found.html"));
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
--- hello-express/routes/admin.js
+++ hello-express/routes/admin.js
@@ -1,19 +1,22 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    `<form action="/product" method="POST">
-        <input type="text" name="title">
-        <button type="submit">Add Product</button>
-      </form>`
-  );
-  // console.log("add product page");
-  // console.log("from:", req.originalUrl, "\n");
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
+//   res.send(
+//     `<form action="/add-product" method="POST">
+//         <input type="text" name="title">
+//         <button type="submit">Add Product</button>
+//       </form>`
+//   );
+// console.log("add product page");
+// console.log("from:", req.originalUrl, "\n");
+// });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   // console.log("product page");
   // console.log("from:", req.originalUrl, "\n");
 
