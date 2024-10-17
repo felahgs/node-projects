349de7065d2eb2b9e90d48c715cbf8c75d1836f5
Felipe Souza <fgsouza93@gmail.com>
2024-10-16 23:37:40 -0300

    mvc: read and save data from file

--- hello-express/models/product.js
+++ hello-express/models/product.js
@@ -1,4 +1,18 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const pathName = require("../util/path");
+
+const p = path.join(pathName, "data", "products.json");
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return callback([]);
+    }
+    return callback(JSON.parse(fileContent));
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -6,12 +20,20 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
-    console.log(`Product saved: ${this.title}`);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        if (!err) {
+          console.log("Product saved to file");
+        } else {
+          console.log("Error saving product to file", err);
+        }
+      });
+    });
   }
 
   // Static methods can be called from the class and not from intance object
-  static fetchAll() {
-    return products;
+  static fetchAll(callback) {
+    getProductsFromFile(callback);
   }
 };
